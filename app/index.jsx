2b519fd2664c6a8213703733cb0822e3477750a6
WGreenwichChaca <walter.greenwich@gmail.com>
2024-11-11 08:09:12 -0500

    Setup Fonts

--- app/index.jsx
+++ app/index.jsx
@@ -6,7 +6,9 @@ import "../global.css";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-red-400">
-      <Text>Aora x 2!</Text>
+      <Text className="text-3xl font-pblack">
+        Baby mi primera app de celular
+      </Text>
       <Link href="/profile" style={{ color: "blue" }}>
         Go to Profile x 2
       </Link>
